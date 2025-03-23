028352ba3d2218574945f49ea052d83c0759e2e3
Denis Ponomarev <star.mods.a@gmail.com>
2025-03-24 00:31:15 +0100

    Basic AI

--- scripts/make-factions-bundle.js
+++ scripts/make-factions-bundle.js
@@ -15,20 +15,20 @@ let mod = await SCParser.createMod({
         '$factions/Umojan.SC2Mod',
         '$factions/UPL.SC2Mod',
         '$factions/ZCR.SC2Mod',
-        '$mods/AllRacesData.SC2Mod'
+        '$mods/AllRaces2-Data.SC2Mod'
     ]
 })
 
 mod.dependencies = [
     'bnet:Void Multi (Mod)/0.0/999,file:Mods/VoidMulti.SC2Mod',
-    'bnet:[ARA] Dragons/0.0/311522,file:Mods/ARC-ASSETS.SC2Mod',
+    'bnet:[AR3] AllRaces-Assets/0.0/332433,file:ASSETS/AllRaces-Assets.SC2Mod',
     'bnet:[AR2] BroodWar Assets/0.0/455593,file:ASSETS/BroodWar-Assets.SC2Mod'
 ]
 
 mod.setDocInfo({
-    Name:`[ARC] All Races PVP Bundle`,
-    DescLong: `Custom Factions Bundle`,
-    DescShort: `1-16`
+    Name:`[AR2] AllRaces2-Bundle`,
+    DescLong: `All Races PvP Factions Data Bundle`,
+    DescShort: ``
 })
 
-mod.write('./../../all-races-pvp-2/AllRaces2-Bundle.SC2Mod')
+mod.write('./../../all-races-pvp-2/AllRaces2-Bundle-New.SC2Mod')
